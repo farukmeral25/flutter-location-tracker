de2608714ced70543e0ef84aff644362ae9aed8e
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-05-25 16:53:28 +0300

    fix: Update Google Maps API key to a test value in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyApk3YrnhGVjMTgwyJusd7XRpKa3pLKs7E")
+    GMSServices.provideAPIKey("TestAPIKey123")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
