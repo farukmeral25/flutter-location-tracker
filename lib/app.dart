13c4958e225716debe5d525f80ea72bcfffeef5f
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-05-25 22:32:40 +0300

    feat: Add AppConstants for app name and integrate it into LocationTrackerApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:marti_case/core/constants/app_constants.dart';
 import 'package:marti_case/core/keys/app_keys.dart';
 import 'package:marti_case/core/utils/routes/app_page_routes.dart';
 
@@ -8,6 +10,8 @@ class LocationTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: AppConstants.appName,
+      debugShowCheckedModeBanner: kDebugMode,
       navigatorKey: AppKeys.navigatorKey,
       scaffoldMessengerKey: AppKeys.scaffoldMessengerKey,
       onGenerateRoute: AppPageRoutes.generateRoute,
